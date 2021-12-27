d73a003b2f66e00dd5f4c8fa9c56c7ef3a2cdd61
Hakeem <ayo75@icloud.com>
2021-12-27 19:34:17 +0000

    Add "remove collection" button

--- client/src/components/charts/Charts.tsx
+++ client/src/components/charts/Charts.tsx
@@ -3,11 +3,14 @@ import Chart from '../chart/Chart';
 import { ChartsProps } from './Charts.types';
 
 
-function Charts({ floorPrice, totalVolume, totalListings }: ChartsProps) {
+function Charts({ floorPrice, totalVolume, totalListings, removeCollection }: ChartsProps) {
     return (
         <div className='space-y-4'>
             <div className='space-y-4'>
-                <h2 className='text-2xl text-gray-100'>Floor price</h2>
+                <div className='flex items-center justify-between'>
+                    <h2 className='text-2xl text-gray-100'>Floor price</h2>
+                    <button className='bg-opacity-10 bg-gray-600 rounded py-2.5 px-5 text-blue-500 hover:text-blue-700 transition-all duration-300' onClick={removeCollection}>Remove collection</button>
+                </div>
                 <Chart value={floorPrice} width={window.screen.width - 80} />
             </div>
             <div className='flex items-center justify-between'>
